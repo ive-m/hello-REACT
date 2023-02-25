2048a5238e08372a2ae4e0effc5f8e96ceda03a8
ive-m <ivesita.maria@gmail.com>
2023-02-25 14:57:09 -0500

    React work input, button

--- src/index.js
+++ src/index.js
@@ -2,23 +2,33 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
+
 const Button = (props) => {
-  // your code here
+  return (<button onClick={props.reset}>RESET</button>);
+};
+
+const Hello = (props) => {
+  if (props.name === "") {
+    return (<h1></h1>);
+  } else {
+    return (<h1>Hello {props.name}</h1>);
+  }
 };
 
 const Application = () => {
-
-  // your code here
+  const [name, setName] = useState("");
 
   const reset = () => {
     console.log("reset");
-    // your code here
+    setName("");
   };
 
   return (
     <main>
-      {/* your code here -- this entire line including the curly braces can be removed */}
-      <h1>Hello React</h1>
+      <input value={name} onChange={(event) => setName(event.target.value)} placeholder="Type your name"></input>
+      <Button reset={reset} />
+      <Hello name={name} />
     </main>
   );
 };
